7823d1ea909024de9ed6378bcffee123d0b5393d
tslatt97 <87451853+tslatt97@users.noreply.github.com>
2021-07-30 15:09:11 +0200

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees.Models
+{
+
+    // Employee objekt
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string EmploymentDate { get; set; }
+    }
+}
